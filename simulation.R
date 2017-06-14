448b81b497ce3ac480fc18584cbd56687cc9407a
spaladin <ivancheyii@gmail.com>
2017-06-15 02:53:52 +0300

    Add files via upload

---
+++ simulation.R
@@ -0,0 +1,19 @@
+# 
+# Simulation
+# 
+
+setwd("~/OneDrive/Models/MacGregor/MacGregor_model1")
+
+source('files/MacGregor_2.R', echo=F)
+
+# simulate 10 attempts of 9-dot problem solution with lookahead = 4
+sim1 <- sim_macgregor(dots_number = 9, attempts = 100, lookahead = 4, k = 1)
+
+# how  many successful solutions?
+sum(sim1$success)
+
+
+# 12 dots, lookahead = 3
+sim2 <- sim_macgregor(dots_number = 12, attempts = 10, lookahead = 3, k = 1)
+sum(sim2$success)
+
